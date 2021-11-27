99bdd3049cdb5b537a6532449fe6dc298574a284
Your usernamemichaelcurry1 <michael.curry@bryanuniversity.edu>
2021-11-27 11:08:26 -0500

    1st

---
+++ week1/getInput.py
@@ -0,0 +1,10 @@
+
+
+city = input("What is your your city: ")
+print("Your name is " + city)
+
+zipcode = input("What is your zip code")
+print("My zip code is" + zipcode )
+
+
+
---
+++ week1/message.py
@@ -0,0 +1,3 @@
+print("I love python")
+print("Python is interesting")
+print("Here are my messages")
