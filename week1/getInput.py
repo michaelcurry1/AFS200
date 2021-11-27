1b0703904e5c6df8a8e4c38e6fd3242a614b73bb
Your usernamemichaelcurry1 <michael.curry@bryanuniversity.edu>
2021-11-27 11:42:52 -0500

    1st

--- week1/getInput.py
+++ week1/getInput.py
@@ -1,10 +1,14 @@
 
 
 city = input("What is your your city: ")
-print("Your name is " + city)
+#print("Your name is " + city)
 
-zipcode = input("What is your zip code")
-print("My zip code is" + zipcode )
+zipcode = input("What is your zip code: ")
+#print("My zip code is" + zipcode )
+
+hometown = city + ', ' + zipcode
+print(hometown)
+
 
 
 
